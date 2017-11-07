f270aaf8efbec50dfabbcd604bc0e60bf121d0f1
Neil Fajardo <docoperator@users.noreply.github.com>
2017-11-07 08:12:42 -0500

    Initial Commit

---
+++ CoreHeap/Heap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreHeap
+{
+    public class Heap
+    {
+        public int[] something;
+
+
+        public int getParentIndex(int index)
+        {
+            int result = 0;
+
+            result = (index - 2) / 2;
+
+            return result;
+        }
+
+        public int getLeftChildIndex(int index)
+        {
+            int result = 0;
+            return result;
+        }
+
+        public bool hasLeftChild(int index)
+        {
+
+
+            return false;
+        }
+
+        public void swapNodes(int firstNode, int secondNode)
+        {
+
+        }
+
+    }
+}
